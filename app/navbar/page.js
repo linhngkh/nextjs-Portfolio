18aa51b91a450034df56764963f77c77f5117472
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-02-27 15:39:17 +0200

    first commit

---
+++ app/navbar/page.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <nav className="fixed w-full top-0 h-16">
+      <ul className="flex justify-evenly p-5 text-2xl capitalize">
+        <li>
+          <Link href="/">linh nguyen</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/projects">My Projects</Link>
+        </li>
+        <li>
+          <Link href="/resume">Resume</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
