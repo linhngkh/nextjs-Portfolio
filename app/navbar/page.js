e42608601ee5cd3e3b94b4978391d3feb5e1d642
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-02-27 16:43:03 +0200

    social icons

--- app/navbar/page.js
+++ app/navbar/page.js
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 
 export const Navbar = () => {
   return (
@@ -11,14 +13,26 @@ export const Navbar = () => {
           <Link href="/about">About</Link>
         </li>
         <li>
-          <Link href="/projects">Projects</Link>
+          <Link href="/projects">projects</Link>
         </li>
         <li>
           <Link href="/resume">Resume</Link>
         </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+        <div className="flex space-x-5">
+          <div>
+            <a href="https://github.com/linh-nguyenkhanh" target="_blank">
+              <BsGithub size="2rem" />
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/linhh-nguyenkhanh/"
+              target="_blank"
+            >
+              <BsLinkedin size="2rem" />
+            </a>
+          </div>
+        </div>
       </ul>
     </nav>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Navbar } from "./navbar/page";
 
+import "../styles/globals.css";
+
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className=" w-screen h-screen flex justify-center items-center text-center">
+      <main className="w-screen h-screen flex justify-center items-center text-center">
         <section className="">
           <div className="uppercase ">
             <h1 className="text-6xl tracking-[.5em] uppercase mt-0 mb-2">
@@ -14,9 +16,10 @@ export default function Home() {
             <p className="text-3xl">junior front end developer</p>
           </div>
         </section>
+
         <footer className="absolute bottom-0 h-16 w-full ">
           {" "}
-          <p> Copyright &#169; 2023 Linh Nguyen All Rights Reserved</p>
+          <h3> &#169; 2023 Linh Nguyen - A Project Built with Next.js</h3>
         </footer>
       </main>
     </>
