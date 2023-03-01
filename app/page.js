2acc097616aa4e7a682cd3c9a7f05a0da6977f88
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-01 20:17:31 +0200

    style footer

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Hero from "@/components/hero/Hero";
 import Navbar from "../components/navbar/Navbar";
 import styles from "../styles/globals.css";
 import { AboutMe } from "@/components/about/AboutMe";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 export default function Home() {
   return (
     <>
@@ -14,9 +15,29 @@ export default function Home() {
       </main>
       <AboutMe />
       {/* footer section */}
-      <footer className=" bottom-0 mt-auto w-full text-center mb-6 ">
-        {" "}
-        <h3> &#169; 2023 Linh Nguyen - A Project Built with Next.js</h3>
+
+      <footer className="footer footer-center p-10 bg-base-200 text-white rounded ">
+        <div className="grid grid-flow-col gap-4">
+          <a className="link link-hover">Home</a>
+          <a className="link link-hover">Resume</a>
+          <a className="link link-hover">Projects</a>
+        </div>
+        <div>
+          <div className="grid grid-flow-col gap-4">
+            <a href="https://github.com/linh-nguyenkhanh" target="_blank">
+              <BsGithub size="2rem" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/linhh-nguyenkhanh/"
+              target="_blank"
+            >
+              <BsLinkedin size="2rem" />
+            </a>
+          </div>
+        </div>
+        <div>
+          <p>Copyright © 2023 Linh Nguyen - A Project Built with Next.js</p>
+        </div>
       </footer>
     </>
   );
