799ab7686999fdc4208fdad157710691159113fc
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-09 21:39:41 +0200

    responsive

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -6,8 +6,8 @@ import { Cube } from "../cube3D/Cube";
 
 const Hero = () => {
   return (
-    <div className="h-screen flex content-center justify-between snap-center mt-10">
-      <section className="h-screen snap-center w-1400 flex justify-between">
+    <div className="bold hero min-h-screen  bg-base-200">
+      <div className="bold hero-content flex-col lg:flex-row">
         <Canvas
           camera={{ fov: 25, position: [5, 5, 5] }}
           className="flex-2 md:flex-1"
@@ -17,29 +17,18 @@ const Hero = () => {
           <directionalLight position={[3, 2, 1]} />
           <Cube />
         </Canvas>
-        <div className="flex-2 flex justify-center items-center flex-col text-start bg-green md:flex-1 ">
-          <div className="mb-7 ">
-            <h1 className=" text-5xl font-bold tracking-[.5em] uppercase leading-loose subpixel-antialiased hover:text-violet-400  ">
-              welcome to <span className="">my space</span>
-            </h1>
-          </div>{" "}
-          <div className="">
-            <div className="mb-7">
-              {" "}
-              <p className="text-3xl text-violet-400 tracking-wider font-medium uppercase hover:drop-shadow-xl  subpixel-antialiased  ">
-                front end web developer
-              </p>
-            </div>
-
-            <div className="mockup-code bg-primary text-primary-content">
-              <pre>
-                <code>Moi! I&apos;m an ex-business student,</code>
-                <code> who loves building things with code.</code>
-              </pre>
-            </div>
+        <div>
+          <h1 className="text-5xl font-bold">WELCOME TO MY PORTFOLIO!</h1>
+          <p className="py-6">FRONT END FOCUSED DEVELOPER</p>
+          <div className="mockup-code bg-primary text-primary-content mb-6">
+            <pre>
+              <code>Moi! I&apos;m an ex-business student,</code>
+              <code> who loves building things with code.</code>
+            </pre>
           </div>
+          <button className="px-10 bold btn bold btn-primary">Hire Me</button>
         </div>
-      </section>
+      </div>
     </div>
   );
 };
