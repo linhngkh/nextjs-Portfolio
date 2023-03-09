3a7d766429372c21682ea2f6b9c82925fbc85948
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-09 21:25:18 +0200

    change about section

--- components/about/AboutMe.js
+++ components/about/AboutMe.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import styles from "../../styles/page.module.css";
 
 export const AboutMe = () => {
   return (
-    <section className={styles.about}>
-      <div className="absolute -z-1 h-3 w-[30%] top-40 left-10 sm:pr-10 sm:top-[200px]">
+    <div className="bold hero min-h-screen bg-base-200">
+      <div className="bold hero-content flex-col lg:flex-row-reverse">
         <Image
           src="/images/IMG-3852.JPG"
           width="300"
@@ -12,32 +11,35 @@ export const AboutMe = () => {
           alt="picture"
           className="m-auto drop-shadow-2xl hover:drop-shadow-xl -z-1 md:max-w-xs  rounded-md -z-1"
         />
-      </div>
+        <div>
+          <h1 className="text-5xl font-bold">About me</h1>
+          <p className="py-6">
+            I first became fascinated with programming last year when I spent
+            time studying UI/UX design. As a person who looks for using
+            aesthetic and efficient applications, I was hooked into coding.
+          </p>
+          <p className="py-6">
+            I research a lot about developer paths and as a marketer before, I
+            decided to follow the path of becoming a Front End Developer. At my
+            university, I did learn HTML, CSS, so I feel picking up Front End
+            stack is pretty cool. Then I spent time learning Javascript at home.
+          </p>
+          <p className="py-6">
+            {" "}
+            I knew that The Shortcut startup opened their Full Stack Academy, so
+            I joined the course and I have gained numerous things about both
+            Front End and Back End. By that, I am deeply more into learning
+            ReactJS framework to build functional apps raise standards for
+            what&apos;s expected of any web application.
+          </p>
 
-      <div className="absolute right-[50px] w-[60%] text-white text-start ml-7 md:max-w-xs  overflow-hidden sm:block   sm:text-md md:overflow-hidden">
-        <p className="text-2xl md:text-lg sm:text-sm">
-          I first became fascinated with programming last year when I spent time
-          studying UI/UX design. As a person who looks for using aesthetic and
-          efficient applications, I was hooked into coding.
-          <br />
-          <br />
-          I research a lot about developer paths and as a marketer before, I
-          decided to follow the path of becoming a Front End Developer. At my
-          university, I did learn HTML, CSS, so I feel picking up Front End
-          stack is pretty cool. Then I spent time learning Javascript at home.
-          <br />
-          <br />
-          I knew that The Shortcut startup opened their Full Stack Academy, so I
-          joined the course and I have gained numerous things about both Front
-          End and Back End. By that, I am deeply more into learning ReactJS
-          framework to build functional apps raise standards for what&apos;s
-          expected of any web application.
-          <br />
-          <br />
-          I&apos;m now seeking a full-time role where I can contribute my skills
-          both in coding and business to help a company achieve their goals.
-        </p>
+          <p className="py-6">
+            I&apos;m now seeking a full-time role where I can contribute my
+            skills both in coding and business to help a company achieve their
+            goals.
+          </p>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
